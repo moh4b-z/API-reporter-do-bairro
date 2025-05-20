7c5465cbe3378b37213d8369a4e5b5f6ef2bace7
moh4b-z <mohammadsalim10000@gmail.com>
2025-05-20 16:12:08 -0300

    SQL da tabela usuarios

---
+++ database/scriptSQL.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE db_controle_jogos_bb;
+
+use db_controle_jogos_bb;
+
+-- Tabela de Usuários
+CREATE TABLE IF NOT EXISTS tbl_usuario (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  senha_salt VARCHAR(32) NOT NULL,
+  senha_hash VARCHAR(128) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  biografia TEXT,
+  data_de_nascimento DATE NOT NULL,
+  nome VARCHAR(50) NOT NULL UNIQUE,
+  foto_perfil VARCHAR(250) NOT NULL
+);
