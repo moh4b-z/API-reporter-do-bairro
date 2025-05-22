99b3eb03636085da6d3f048ee914cd7783dc2e0f
Kauan Rodrigues <kauan.leonel.rodrigues@gmail.com>
2025-05-22 10:38:30 -0300

    Script Usuário feito

--- database/scriptSQL.sql
+++ database/scriptSQL.sql
@@ -2,14 +2,59 @@ CREATE DATABASE db_controle_bairro;
 
 use db_controle_bairro;
 
--- Tabela de Usuários
-CREATE TABLE IF NOT EXISTS tbl_usuario (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  senha_salt VARCHAR(32) NOT NULL,
-  senha_hash VARCHAR(128) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  biografia TEXT,
-  data_de_nascimento DATE NOT NULL,
-  nome VARCHAR(50) NOT NULL UNIQUE,
-  foto_perfil VARCHAR(250) NOT NULL
+-- Tabela: tbl_usuario
+CREATE TABLE tbl_usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    senha_salt VARCHAR(32),
+    senha_hash VARCHAR(128),
+    email VARCHAR(250),
+    data_nascimento DATE,
+    foto_perfil VARCHAR(300),
+    biografia VARCHAR(300)
+);
+
+-- Tabela: tbl_midia
+CREATE TABLE tbl_midia (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url_img VARCHAR(45),
+    url_videos VARCHAR(45)
+);
+
+-- Tabela: categoria
+CREATE TABLE categoria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    descricao VARCHAR(200),
+    sigla VARCHAR(5)
+);
+
+-- Tabela: tbl_noticia
+CREATE TABLE tbl_noticia (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100),
+    conteudo TEXT,
+    endereco VARCHAR(300),
+    lon DECIMAL(11,7),
+    lat DECIMAL(10,7),
+    tbl_usuario_id INT,
+    FOREIGN KEY (tbl_usuario_id) REFERENCES tbl_usuario(id)
+);
+
+-- Tabela: tbl_midia_noticia (tabela associativa entre notícia e mídia)
+CREATE TABLE tbl_midia_noticia (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tbl_noticia_id INT,
+    tbl_midia_id INT,
+    FOREIGN KEY (tbl_noticia_id) REFERENCES tbl_noticia(id),
+    FOREIGN KEY (tbl_midia_id) REFERENCES tbl_midia(id)
+);
+
+-- Tabela: tbl_noticia_categoria (tabela associativa entre notícia e categoria)
+CREATE TABLE tbl_noticia_categoria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tbl_noticia_id INT,
+    categoria_id INT,
+    FOREIGN KEY (tbl_noticia_id) REFERENCES tbl_noticia(id),
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
 );
